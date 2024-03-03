1bc434b80e8a7a1a2e0ba1007a27e0ce1a7038c4
Jesse French <frejes32@gmail.com>
2024-03-03 11:02:12 -0600

    Added READ/GET functions to show cards on dom

--- components/shared/domBuilder.js
+++ components/shared/domBuilder.js
@@ -3,8 +3,10 @@ import renderToDOM from '../../utils/renderToDom';
 const domBuilder = () => {
   const domString = `
   <div id="navigation"></div>
-  <div id="filter"></div>
-  <div id="main-container"></div>`;
+  <div id="main-container">
+    <div id="filter"></div>
+    <div id="cards"></div>
+  </div>`;
   renderToDOM('#app', domString);
 };
 
