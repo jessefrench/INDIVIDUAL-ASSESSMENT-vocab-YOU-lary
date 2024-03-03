d677472f1b0bdba6db612a0296d8eea65af4acde
Jesse French <frejes32@gmail.com>
2024-03-02 21:38:24 -0600

    Added filter buttons to sort cards by language/tech

--- components/shared/domBuilder.js
+++ components/shared/domBuilder.js
@@ -3,10 +3,8 @@ import renderToDOM from '../../utils/renderToDom';
 const domBuilder = () => {
   const domString = `
   <div id="navigation"></div>
-  <div id="main-container">
-    
-  </div>`;
-
+  <div id="filter"></div>
+  <div id="main-container"></div>`;
   renderToDOM('#app', domString);
 };
 
