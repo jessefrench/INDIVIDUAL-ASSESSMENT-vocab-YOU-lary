517f1343ea99cd550b0e9d5c932bc60925302e2c
Jesse French <frejes32@gmail.com>
2024-03-09 11:55:27 -0600

    Fixed login/logout appearance

--- components/buttons/loginButton.js
+++ components/buttons/loginButton.js
@@ -2,10 +2,9 @@ import { signIn } from '../../utils/auth';
 
 const loginButton = () => {
   const domString = `
-    <h1 class="card-title">Welcome to vocab-YOU-lary!</h1>
-    <button id="google-auth" class="btn btn-success">Login</button>`;
-
-  document.querySelector('#login-form-container').innerHTML = domString;
+  <h1 class="card-title">Welcome to vocab-YOU-lary!</h1>
+  <button id="google-auth" class="btn btn-success">Login</button>`;
+  document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
 
